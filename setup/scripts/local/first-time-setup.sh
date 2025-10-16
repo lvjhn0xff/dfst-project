f4d325c4b768b525c92ae53ec4fa9e11b07e81e7
lvjhn0xff <lvjhn.mx@gmail.com>
2025-10-17 00:55:03 +0800

    general update

--- setup/scripts/local/first-time-setup.sh
+++ setup/scripts/local/first-time-setup.sh
@@ -2,14 +2,23 @@
 
 source setup/scripts/local/@local-context 
 
+# Add backend. 
+if [ ! -d setup/services/local/backend ]; then
+    bash setup/utils/projects/backends/add-backend.sh
+fi
+
 # Build images. 
 bash setup/scripts/local/build.sh 
 
 # Create containers. 
 bash setup/scripts/local/create.sh
 
-# Install dependencies in front-end.
-source setup/scripts/local/setups/setup-frontend.sh
 
+# Install dependencies in front-end.
+if [ "$PROJECT_FRONTEND_ONLY" == true ]; then
+    source setup/scripts/local/setups/setup-frontend.sh
+else 
+    echo ""
+fi
 
 
--- setup/utils/project/unlink-source.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm -f ./source/backend
